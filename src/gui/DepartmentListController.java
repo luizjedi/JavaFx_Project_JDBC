0ace9e5198064683cfc2dda2eac9a6dbfca6011d
luizjedi <luiz.pepe@hotmail.com>
2021-05-14 19:42:05 -0300

    Update department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -9,6 +9,7 @@ import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -44,6 +46,9 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	@FXML
 	private TableColumn<Department, String> tcName;
 
+	@FXML
+	private TableColumn<Department, Department> tcEDIT;
+
 	private ObservableList<Department> obsList;
 
 	public void setDepartmentService(DepartmentService service) {
@@ -60,16 +65,6 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	@FXML
 	public void onTableViewDepartmentAction() {
 		System.out.println("onTableViewDepartmentAction");
-	}
-
-	@FXML
-	public void onTableColumnIdAction() {
-		// System.out.println("onTableColumnIdAction");
-	}
-
-	@FXML
-	public void onTableColumnNameAction() {
-		// System.out.println("onTableColumnNameAction");
 	}
 
 	@Override
@@ -96,6 +91,8 @@ public class DepartmentListController implements Initializable, DataChangeListen
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tvDepartment.setItems(obsList);
+		initEditButtons();
+		
 
 	}
 
@@ -130,6 +127,28 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	@Override
 	public void onDataChanged() {
 		updateTableView();
+	}
+
+	private void initEditButtons() {
+		
+		tcEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tcEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+			private final Button button = new Button("Edit");
+
+			@Override
+			protected void updateItem(Department obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj,
+								"/gui/DepartmentForm.fxml", 
+								Utils.currentStage(event)));
+			}
+		});
 	}
 
 }
